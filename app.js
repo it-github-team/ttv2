e19f996991cddf3c1eb3c026abe5f20cbcb25871
Sueno <RAFAEL.SUENO@int.asurion.com>
2017-01-21 00:11:57 +0800

    changes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var multer = require('multer');
-var upload = multer(); 
+var upload = multer();
+var logger = require('winston');
 var app = express();
 
-var home = require('./ctrl/home.js');
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 18080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var ipaddress;
@@ -15,15 +14,19 @@ var port;
 
 function initIPAdress() {
     var adr = process.env.OPENSHIFT_NODEJS_IP;
+    port = process.env.OPENSHIFT_NODEJS_PORT;
     if (typeof adr === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
             console.warn('No OPENSHIFT_NODEJS_IP var, using localhost');
             adr = 'localhost';
+            port = 18080;
     }
-    port = process.env.OPENSHIFT_NODEJS_PORT;
     ipaddress = adr;
 }
+
+logger.add(logger.transports.File, { filename: 'tabletop.log'});
+logger.info('Logger initialized successfully...');
 
 app.set('view engine', 'ejs');
 app.set('views','./view');
@@ -36,7 +39,11 @@ app.use(upload.array()); // for parsing multipart/form-data
 
 app.use(cookieParser());
 
+var home = require('./ctrl/home.js');
+var games = require('./ctrl/games.js');
+
 app.use('/home', home);
+app.use('/games', games);
 
 app.get('/', function(req, res) {
   res.redirect(301, '/home/');
--- ctrl/home.js
+++ ctrl/home.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('template/base', {viewVar: 'index'});
+  res.render('template/base', {base: 'home', viewVar: 'index'});
 });
 
 
 router.get('/products', function(req, res) {
-  res.render('template/base', {viewVar: 'products'});
+  res.render('template/base', {base: 'home', viewVar: 'products'});
 });
 
 
 router.get('/contact', function(req, res) {
-  res.render('template/base', {viewVar: 'contact'});
+  res.render('template/base', {base: 'home', viewVar: 'contact'});
 });
 
 
 router.get('/about', function(req, res) {
-  res.render('template/base', {viewVar: 'about'});
+  res.render('template/base', {base: 'home', viewVar: 'about'});
 });
 
 module.exports = router;
