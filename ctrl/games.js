ee98cdbca90cfdab94004fa9fe9c5bf4a7b3ca78
Sueno <RAFAEL.SUENO@int.asurion.com>
2017-04-12 10:21:17 +0800

    just pushing changes. dont really know what I changed

--- ctrl/games.js
+++ ctrl/games.js
@@ -2,20 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('template/base', {base: 'home', viewVar: 'index'});
+  res.redirect(301, '/home/products');
 });
+
 
 router.get('/products', function(req, res) {
   res.render('template/base', {base: 'home', viewVar: 'products'});
 });
 
-router.get('/delivery', function(req, res) {
-  res.render('template/base', {base: 'home', viewVar: 'delivery'});
-});
 
 router.get('/contact', function(req, res) {
   res.render('template/base', {base: 'home', viewVar: 'contact'});
 });
+
 
 router.get('/about', function(req, res) {
   res.render('template/base', {base: 'home', viewVar: 'about'});
